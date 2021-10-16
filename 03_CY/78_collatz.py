cfd2fb6af792fdb7f73c5662ff4d59b2d21ad8af
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-10-16 22:35:28 +0300

    Update 78_collatz.py

--- 03_CY/78_collatz.py
+++ 03_CY/78_collatz.py
@@ -1,16 +1,20 @@
 from math import floor, ceil
-last_element = float(input("Enter any positive number: "))
+last_element = input("Enter any positive number: ")
+if last_element == "" :
+    quit()
+last_element = float(last_element)
 
 
 while last_element != 1 :
             print(last_element)
+            last_element = float(last_element)
             if last_element % 2 == 0 :
                 last_element = floor(last_element/2)
             elif last_element % 2 != 0 :
                 last_element = ceil(last_element*3 + 1)
             if last_element == 1 :
                 print(last_element)
-                last_element = float(input("enter the new number: "))
-            if last_element == 0 :
+                last_element = input("enter the new number: ")
+            if last_element == 0 or last_element == "":
                 quit()
 
