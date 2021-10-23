6818e256dd81932b69f0636d85ba8c5080f7d7ca
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-10-23 19:39:40 +0300

    Update p93_center_line.py

--- 04_DE/p93_center_line.py
+++ 04_DE/p93_center_line.py
@@ -3,7 +3,7 @@ def center(s,w):
     if len(s) >= w :
        return s
     else :
-        right_and_left_indent = ceil(w - len(s))/2)
+        right_and_left_indent = ceil(w - len(s)/2)
         s = (" "*right_and_left_indent+s+" "*right_and_left_indent)
         return s
 def main() :
