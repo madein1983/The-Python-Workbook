d7045ad9dd2415adc602b779392b12f4ed2f557f
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-10-25 20:53:31 +0300

    Update p100_random.py

--- 04_DE/p100_random.py
+++ 04_DE/p100_random.py
@@ -1,14 +1,17 @@
 from random import randint
 def curr_password():
-    pass_length = randint(7,10)
-    i = 0
-    password = ""
-    for i in range(pass_length-1):
-        curr_ord = randint(48,126)
-        password = password + chr(curr_ord)
-    print(password)
+  
+        pass_length = randint(7,10)
+        i = 0
+        password = ""
+        for i in range(pass_length-1):
+            curr_ord = randint(48,126)
+            password = password + chr(curr_ord)
+        # print(password)
+        return password
 
 def main():
+    
     curr_password()
 
 if __name__ == "__main__":
