a44dd2fba02603bd5be3478d9e247c14e55fd303
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-10-31 20:17:27 +0200

    Create p117_words_only.py

---
+++ 05_LI/p117_words_only.py
@@ -0,0 +1,16 @@
+def srting_to_list (line_in) :
+    print("7")
+
+def allchars() :
+    for i in range(0,500) :
+        print("|_ ", i, " _|_ ",chr(i), "_|")
+def 
+
+def main():
+    line_in = str(input("Enter sentence: "))
+    # print(allchars())
+
+    
+    # print(string.printable)
+if __name__ == "__main__" :
+    main()
