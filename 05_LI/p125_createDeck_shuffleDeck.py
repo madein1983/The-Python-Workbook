f2a08d2415786050e5269a64af1dcb1e326dad91
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-05 22:25:58 +0200

    Update p125_createDeck_shuffleDeck.py

--- 05_LI/p125_createDeck_shuffleDeck.py
+++ 05_LI/p125_createDeck_shuffleDeck.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import shuffle, randint
 
 def createDeck() :
     nominal = ["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
@@ -9,19 +9,29 @@ def createDeck() :
         for j in range(0,13) : 
             temp_deck = temp_deck + suit[i] + nominal[j] + " "
     deck = temp_deck.split()
-    print(deck)
+    print("Initial deck:    ",deck)
     return deck
 
 def shuffle_resident(deck) :
     shuffle(deck)
-    print(deck)
+    print("Standart shuffle:", deck)
     return deck
 
 def shuffle_new(deck) :
-    print()
+    for i in range(0,len(deck)) :
+        random_integer = randint(i,len(deck))
+        if random_integer + i >= len(deck) :
+            random_integer = - random_integer
+        temp = deck[i] 
+        deck[i] = deck[random_integer] 
+        deck[random_integer] = temp
+    print("My shuffle      :",deck)
+    return deck
+
 def main() :
     deck = createDeck()
     shuffle_resident(deck)
+    shuffle_new(deck)
 
 
 if __name__ == "__main__" :
