000e0c163c3f38b2c9e80cd481d5c7797dfb7ad5
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-10 17:37:32 +0200

    Create p130_unary_and_binary_ops.py

---
+++ 05_LI/p130_unary_and_binary_ops.py
@@ -0,0 +1,22 @@
+from p129_division_into_tokens import token_division
+
+def unary_excretion (l_o_t) : # split_off
+    list_out = []
+    for i in range(0, len(l_o_t)) :
+        if i == 0 and (l_o_t[i] == "+") :
+            list_out.append("u+")
+        elif i == 0 and (l_o_t[i] == "-") :
+            list_out.append("u-")
+        elif (l_o_t[i] == "-" ) and (l_o_t[i-1] == "*" or l_o_t[i-1] == "/" or l_o_t[i-1] == "-" or l_o_t[i-1] == "+") :
+            list_out.append("u-")
+        elif (l_o_t[i] == "+" ) and (l_o_t[i-1] == "*" or l_o_t[i-1] == "/" or l_o_t[i-1] == "-" or l_o_t[i-1] == "+") :
+            list_out.append("u+")
+        else :
+            list_out.append(l_o_t[i])
+    print(list_out)
+def main() :
+    line_in = input("Enter the string: ")
+    unary_excretion(token_division(line_in))
+
+if __name__ == "__main__" :
+    main()
