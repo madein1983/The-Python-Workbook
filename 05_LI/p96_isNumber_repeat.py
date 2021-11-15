b7c9488dffed0747a7671aad334ead99949b58eb
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-15 20:05:55 +0200

    Create p96_isNumber_repeat.py

---
+++ 05_LI/p96_isNumber_repeat.py
@@ -0,0 +1,19 @@
+def isInteger(x) :
+    # x_new = x.strip(" ")
+    i = 0 
+    digit_count = 0
+    oth_symbols_count = 0
+    x = x.strip()
+    while i <= len(x)-1 :
+        if ord("0") <= ord(x[i]) and ord(x[i])<= ord("9") :
+            digit_count +=1
+        else :
+            oth_symbols_count +=1
+        i+=1
+    print(digit_count," : ",oth_symbols_count)
+    if oth_symbols_count == 0 and digit_count >0 :
+        return True
+    elif oth_symbols_count > 0 and digit_count == 0 :
+        return False
+    elif oth_symbols_count >0 and digit_count > 0 :
+        return False
