4f03874421c15b9108106521d8d424a4c3aa4858
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-16 17:14:38 +0200

    rename directories

---
+++ 01_INPUT/1.py
@@ -0,0 +1,23 @@
+## Displays the first name of user and his ZIP code
+#
+#
+f_name = "Alex"
+post_code = 65014
+print(f_name,post_code)
+#####################################################################################################################################################
+## Input requere and say hello
+#
+#
+#n=""
+#j = input('Enter the Name ' + n)
+#print('Hello ', j)
+#####################################################################################################################################################
+## Calculation square of the room
+# with floating 
+#
+print('hi there lets evaluate the square!')
+a = float(input('enter the side a = '))
+b = float(input('enter the side b = '))
+S = a * b
+print('Вывод в формате float с точностью 2 десятых' "%.2f" %S)
+#####################################################################################################################################################
