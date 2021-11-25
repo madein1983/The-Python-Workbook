23887877b248ab9d8a99b5f8c1307befe84292bb
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-25 22:13:21 +0200

    Create p150_head_of_file.py

---
+++ 07_FILES_AND_EXCEPTONS/p150_head_of_file.py
@@ -0,0 +1,22 @@
+import sys
+NUM_OF_LINES = 10
+
+if len(sys.argv) != 2 :
+    print("there is two  arguments needed!")
+    quit()
+
+try:
+    inf = open(sys.argv[1],"r")
+    file_opened = True
+    line = inf.readline()
+    num = 0
+    while num < NUM_OF_LINES and line != "":
+        line = line.rstrip()
+        num +=1
+        print(line)
+        line = inf.readline()
+    inf.close()
+
+except IOError:
+    print("IOError. Exit")
+    quit()
