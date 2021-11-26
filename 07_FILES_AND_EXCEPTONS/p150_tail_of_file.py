4c124bde1139fdfc4001d0cb8989f89bc7b1bebd
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-26 13:22:56 +0200

    removed mistakes

--- 07_FILES_AND_EXCEPTONS/p150_tail_of_file.py
+++ 07_FILES_AND_EXCEPTONS/p150_tail_of_file.py
@@ -1,12 +1,12 @@
+from os import close
 import sys
-
 if len(sys.argv) != 2 :
     print("count of argv != 2 quit()")
     quit()
-
 file_cont = open(sys.argv[1],"r")
 file_in_list = []
-
+print(sys.argv[0])
+print(sys.argv[1])
 try :
     line_in = file_cont.readline()
     line_in.strip()
@@ -20,7 +20,7 @@ try :
     for i in range(len(file_in_list)-10,len(file_in_list)):
         print(file_in_list[i])
     # print(file_in_list)
-
+    file_cont.close() 
 except IOError:
     print("IOError")
     quit()
---
+++ 07_FILES_AND_EXCEPTONS/p151_cat_files.py
@@ -0,0 +1,19 @@
+import sys
+
+if len(sys.argv) < 2 :
+    print("Incorrect input")
+print("The rersult of concatenation of: ")
+# for i in range(1,len(sys.argv)) :
+#     print(sys.argv[i])
+#### equivalent to:
+# for j in sys.argv :
+#     print(j)
+for i in range(1,len(sys.argv)):
+    print("The fiel name is: ",sys.argv[i])
+    file_container = open(sys.argv[i])
+    line_from_file = file_container.readline()
+    while line_from_file != "" :
+        print(line_from_file)
+        line_from_file = file_container.readline()
+    file_container.close()
+    
