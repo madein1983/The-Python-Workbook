d6285a09b0909796646353c7f5d7afe8155a5f50
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-11-28 21:02:36 +0200

    Create p154_freq_of_symb.py

---
+++ 07_FILES_AND_EXCEPTONS/p154_freq_of_symb.py
@@ -0,0 +1,26 @@
+import  sys
+
+alphabet = {}
+for i in range(65,91):
+    alphabet[chr(i)] = 0
+# print(alphabet)
+if len(sys.argv) != 2:
+    print("argv error")
+file_container = open(sys.argv[1],"r")
+lines = file_container.readlines()
+# print(lines) #add al lines in list
+# print(type(lines))
+file_container.close()
+
+lines1 = []
+for i in lines :
+    lines1.append(i[:-2])
+
+
+for i in lines:
+    for j in i:
+        if j.upper() in alphabet:
+           alphabet[j.upper()] = alphabet[j.upper()] + 1  
+print(alphabet)
+for i in alphabet:
+    print(i, " : ", alphabet[i])
