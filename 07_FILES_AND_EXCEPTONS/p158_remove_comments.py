0a27ef027faefe3e1c8991740c5152054c14b54b
Oleksii Pokotylov <aleksey.pokotilov@gmail.com>
2021-12-01 19:36:21 +0200

    p158

---
+++ 07_FILES_AND_EXCEPTONS/p158_remove_comments.py
@@ -0,0 +1,36 @@
+import sys 
+
+
+if len(sys.argv) != 2 :
+    print("only one file as argument ") 
+file_in_lines = []
+
+try:
+    file_container = open(sys.argv[1])
+    line = file_container.readline()
+    file_in_lines.append(line)
+    while line != "":
+        line = file_container.readline()
+        if line != "":
+            file_in_lines.append(line)
+    file_container.close()
+    print(file_in_lines)
+# extract hash symbol
+    new_file_in_lines = []
+    for i in file_in_lines :
+            if i[0] != "#" :
+               new_file_in_lines.append(i)
+            
+    print(new_file_in_lines)
+
+    new_file_container=open(sys.argv[1]+".new","w")
+    for i in new_file_in_lines:
+        i = i+"\n"
+        new_file_container.write(i)
+    new_file_container.close()
+
+except IOError:
+    print("IOError")
+
+
+
